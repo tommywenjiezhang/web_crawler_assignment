0bb489527ac72361244dc4a8637edd8f0921a037
Wenjie Zhang <wenjiezhang@wenjies-mbp.resource.campus.njit.edu>
2022-04-13 17:46:31 -0400

    Assignment commit one revolved issue

--- Crawler Assignment.R
+++ Crawler Assignment.R
@@ -1,5 +1,5 @@
 #Write a topical crawler using the information provided below:
-list.of.packages <- c("xml2")
+list.of.packages <- c("xml", "RCurl", "stringr", "httr", "xml2", "httr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -10,6 +10,7 @@ library(XML)
 library(stringr)
 library(httr)
 library(xml2)
+library(stringi)
 
 
 htmlToText <- function(input, ...) {
@@ -67,12 +68,36 @@ htmlToText <- function(input, ...) {
 
 
 
+
+invalid_utf8_ <- function(x){
+  
+  !is.na(x) & is.na(iconv(x, "UTF-8", "UTF-8"))
+  
+}
+
+detect_invalid_utf8 <- function(string, seperator){
+  
+  stringSplit <- unlist(strsplit(string, seperator))
+  
+  invalidIndex <- unlist(lapply(stringSplit, invalid_utf8_))
+  
+  data.frame(
+    word = stringSplit[invalidIndex],
+    stringIndex = which(invalidIndex == TRUE)
+  )
+  
+}
+
+
+
 ###Run the function code for htmlToText()(Be sure this function is listed in your Environment)
 
 ###Load the first element in the frontier to an "exploredlink" variable
 
 
-frontier <- c("https://www.cnn.com","https://www.kdnuggets.com/")
+frontier <- c("https://www.cnn.com","https://www.kdnuggets.com/","http://news.google.com")
+
+
 
 topicwords<-c("technology","school","web","mining","news")
 
@@ -82,6 +107,7 @@ j <- 0  #number of items in the repository
 
 
 while (j < num){
+  
 
   if(length(frontier)<1){
     break
@@ -99,10 +125,16 @@ while (j < num){
   
   #fill in your code here
   #How to get HTML
-  doc <- tryCatch(getURL(exploredlink),error=function(cond){return("")})
+  doc <- tryCatch(getURL(exploredlink,.encoding = 'UTF-8'),error=function(cond){return("")})
+  
+  if(invalid_utf8_(doc)){
+    print("invalid page ")
+    next
+  }
   
   
-  if(str_length(doc)<10){
+  if(tryCatch(str_length(doc), error=function(cond){return(0)})<10){
+    print("<10")
     next
   }
   
@@ -110,14 +142,14 @@ while (j < num){
   
   domain<-str_extract(exploredlink,pattern = ".*\\.com")
   
-  if(is.na(domain)){
+  if(is.null(domain)){
     next
   }
   
   #How to get a title
   titleText <- tryCatch(xmlToDataFrame(nodes = getNodeSet(doc, "//title")),error=function(cond){return("")})
   
-  if(is.na(titleText) ){
+  if(is.null(titleText) ){
     next
   }
   titleText <- as.vector(titleText$text)
@@ -125,21 +157,20 @@ while (j < num){
   titleText <- paste(str_trim(str_replace_all(titleText, "[^[:alnum:]]", " ")), collapse = '')
   
   #How to get body text 
-  htmlContext <- content(GET(exploredlink),type="text/html",as="text")
-  bodyText<- tryCatch(htmlToText(htmlContext),error=function(cond){return("")})
   
+  bodyText<- tryCatch(stri_enc_toutf8(htmlToText(content(GET(exploredlink),type="text/html",as="text"))),error=function(cond){return("")})
   bodyText<-str_split(tolower(str_replace_all((str_replace_all(bodyText,"(\\t|\\n|\\r)"," ")),"\\s{2,}"," "))," ")[[1]]
-  
   ###  write to file
   for (val in topicwords)
   {
     if(val %in% bodyText){
       path <- paste(titleText,".html")
-      write.table(tryCatch(htmlToText(htmlContext),error=function(cond){return("")}), 
+      write.table(bodyText, 
                   file= path, 
                   quote = FALSE,
                   col.names = FALSE,
                   row.names = FALSE)
+      write(c(exploredlink,titleText), file="repositary.txt",append=TRUE,sep = "\t")
       next
     }
   }
@@ -174,10 +205,16 @@ while (j < num){
   }
   
   
-  
+
 }
 
 
+
+
+
+# show number of html pages 
+library(system)
+system("echo 'number of html pages: ' $(ls -l *.html | wc -l) ")
 
 ############   USEFUL CODE SNIPPETS ########
 
